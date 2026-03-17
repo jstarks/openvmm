6f1b5dcdba7befdd598a15fc2dced4bc20ee8108
John Starks <jostarks@microsoft.com>
2026-03-17 02:34:06 +0000

    revert virtio bar0 changes

--- vm/devices/virtio/virtio/src/transport/mmio.rs
+++ vm/devices/virtio/virtio/src/transport/mmio.rs
@@ -164,8 +164,9 @@ impl VirtioMmioDevice {
 
 impl VirtioMmioDevice {
     pub(crate) fn read_u32(&mut self, address: u64) -> u32 {
-        assert!(address & 3 == 0);
-        match VirtioMmioRegister(address) {
+        let offset = (address & 0xfff) as u16;
+        assert!(offset & 3 == 0);
+        match VirtioMmioRegister(offset) {
             VirtioMmioRegister::MAGIC_VALUE => u32::from_le_bytes(*b"virt"),
             VirtioMmioRegister::VERSION => 2,
             VirtioMmioRegister::DEVICE_ID => self.device_id,
@@ -271,7 +272,7 @@ impl VirtioMmioDevice {
     }
 
     pub(crate) fn write_u32(&mut self, address: u64, val: u32) {
-        let offset = address & 0xfff;
+        let offset = (address & 0xfff) as u16;
         assert!(offset & 3 == 0);
         let queue_select = self.queue_select as usize;
         let queues_locked = self.device_status.driver_ok();
@@ -361,7 +362,7 @@ impl VirtioMmioDevice {
 
                 if !self.device_status.driver_ok() && new_status.driver_ok() {
                     let notification_address =
-                        (address & !0xfff) + VirtioMmioRegister::QUEUE_NOTIFY.0;
+                        (address & !0xfff) + VirtioMmioRegister::QUEUE_NOTIFY.0 as u64;
                     for i in 0..self.events.len() {
                         self.doorbells.add(
                             notification_address,

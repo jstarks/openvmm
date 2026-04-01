934a69b5867370ce6212985c16a43a1d26ed57db
John Starks <jostarks@microsoft.com>
2026-04-01 01:00:34 +0000

    petri/openvmm: enable transparent hugepages for guest memory

--- petri/src/vm/openvmm/construct.rs
+++ petri/src/vm/openvmm/construct.rs
@@ -351,7 +351,7 @@ impl PetriVmConfigOpenVmm {
                 pci_mmio_gaps: vec![],
                 prefetch_memory: false,
                 private_memory: false,
-                transparent_hugepages: false,
+                transparent_hugepages: true,
             }
         };
 
